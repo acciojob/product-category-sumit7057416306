97f29f8fd4a9b260a4d647e4d4e59bc1688b6188
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-04-02 19:25:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 
-import React from "react";
+import React,  { useState }  from "react";
 import './../styles/App.css';
+import ItemList from "./ItemList";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Male");
+  const [categories, setCategories] = useState({
+    Male: ["Shirts", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"],
+    Female: ["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"]
+  });
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <select
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        {Object.keys(categories).map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <ItemList items={categories[selectedCategory]} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
